c101655b192145ce79f398a5e8c7ac249587629b
moononmoon <70292146+moononmoon@users.noreply.github.com>
2020-08-31 00:20:49 -0500

    complete

--- assignments/A03/11498/main.cpp
+++ assignments/A03/11498/main.cpp
@@ -1,1 +1,47 @@
 
+/*
+Author: Mark Tachiri
+Email:  marktachiri@ymail.com
+Label:  11498
+Title:  11498 - Division of Nlogonia
+Course: 4883
+Semester: Fall 2020
+
+Description:  this program determines what direction a certain point is at given the main point
+*/
+#include <iostream>
+
+using namespace std;
+int main() {
+  int k,n,m,x,y;
+  cin >> k;
+  while (k != 0)
+  {
+  cin >> n >> m;
+  for(int i = 0; i<k; i++)
+  {
+    cin >> x >> y;
+    if(x < n & y < m)
+    {
+      cout << "SO" <<endl;
+    }
+    else if(x > n & y > m)
+    {
+      cout << "NE" <<endl;
+    }
+    else if(x < n & y > m)
+    {
+      cout << "NO" <<endl;
+    }
+    else if(x > n & y < m)
+    {
+      cout << "SE" <<endl;
+    }
+    else
+    {
+      cout << "divisa\n";
+    }
+  }
+  cin >> k;
+  }
+}
