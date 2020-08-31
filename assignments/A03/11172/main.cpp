4d31636f1e2a664343e5e8c7c5349b82dc2ad4c4
moononmoon <70292146+moononmoon@users.noreply.github.com>
2020-08-31 01:18:44 -0500

    complete

--- assignments/A03/11172/main.cpp
+++ assignments/A03/11172/main.cpp
@@ -1,1 +1,30 @@
+/*
+Author: Mark Tachiri
+Email:  marktachiri@ymail.com
+Label:  11172
+Title:  11172 - Relational Operator
+Course: 4883
+Semester: Fall 2020
 
+Description:  this program determines whether a number is greater than, less than or equal to the other number
+*/
+#include <iostream>
+
+using namespace std;
+int main() {
+  int k,n,m;
+  cin >> k;
+  for(int i = 0; i<k; i++)
+  {
+    cin>>n>>m;
+    if(n>m){
+      cout<<">"<<endl;
+    }
+    else if (n < m){
+      cout<<"<"<<endl;
+    }
+    else{
+      cout<<"=\n";
+    }
+  }
+}
