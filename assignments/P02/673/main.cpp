9b199985f5749dd2aa9b229cc41fcf9c74508191
moononmoon <70292146+moononmoon@users.noreply.github.com>
2020-09-03 20:37:47 -0500

    complete

--- assignments/P02/673/main.cpp
+++ assignments/P02/673/main.cpp
@@ -1,1 +1,45 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+int main()
+{
+     string chr;
+  int t,length;
+  cin>>t;
+    getchar();
+    for (int j = 0;j<t;j++)
+    {
+        getline(cin,chr);
+        length=chr.length();//get the length of the string
+        stack<char>stck;//create stack
+        if(length%2!=0)
+            cout<<"No"<<endl;
+
+        else
+        {
+
+            for(int i=0; i<length; i++)
+            {
+                if(chr[i]=='('||chr[i]=='[')
+                {
+                    stck.push(chr[i]);
+                }
+               else if((!stck.empty()&&chr[i]==')'&&stck.top()=='(')|| (!stck.empty()&&chr[i]==']'&&stck.top()=='['))
+                {
+                    stck.pop();
+                }
+                else
+                {
+                    stck.push(chr[i]);
+                }
+            }
+
+            if(stck.empty())
+                cout<<"Yes"<<endl;
+            else
+                cout<<"No"<<endl;
+        }
+    }
+
+    return 0;
+}
